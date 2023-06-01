7e23da9cb8ec4b1042f27e8ac97ce82a46ef36ba
Ruchi Bhandari <98799210+ruchibhandarigithub@users.noreply.github.com>
2023-06-01 11:21:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 // complete the given function
 
 function palindrome(str){
+	  // Remove non-alphanumeric characters and convert to lowercase
+  str = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+  
+
 	let reversed = str.split(' ').reverse().join(' ');
 	return str === reversed;
 
