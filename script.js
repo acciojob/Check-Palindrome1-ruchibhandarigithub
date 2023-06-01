c72bb256018aa771f23bfdf87289648ec97e3a01
Ruchi Bhandari <98799210+ruchibhandarigithub@users.noreply.github.com>
2023-06-01 11:27:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,10 +2,10 @@
 
 function palindrome(str){
 	  // Remove non-alphanumeric characters and convert to lowercase
-  str = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+ // str = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
   
 
-	let reversed = str.split(' ').reverse().join(' ');
+	let reversed = str.split('').reverse().join('');
 	return str === reversed;
 
 }
